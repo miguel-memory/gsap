18372882d5863a2d1173c27b1b150f8aa257267a
Miguel <miguel@memory.ai>
2023-01-02 19:27:01 -0400

    install

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import animateTitle from './features/animateTitle'
+import createBadge from './features/createBasge'
+import './styles/style.css'
+
+console.log('Welcome to Vite + JS + Webflow!')
+
+createBadge()
+animateTitle()
+
+import gsap from 'gsap'
+
+
