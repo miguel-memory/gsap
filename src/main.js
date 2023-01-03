13aedf126eb579ddf54c0b1a352e8b290271650a
Miguel <miguel@memory.ai>
2023-01-02 20:49:14 -0400

    gsap installed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
-
-console.log('Welcome to Vite + JS + Webflow!')
-
-createBadge()
-animateTitle()
-
 import gsap from 'gsap'
 
-
+gsap.to('.heading-huge', { xPercent: 100, duration: 3 })
