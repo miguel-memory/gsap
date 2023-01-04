15db0b9bc8430ac4a0ca4e95413416241fe49ec8
Miguel <miguel@memory.ai>
2023-01-04 15:47:36 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,6 @@ console.log('this is working')
 
 import gsap from 'gsap'
 
-gsap.to('.heading-huge', { xPercent: 100, duration: 3, ease: 'power2.out' })
+var mySplitText = new SplitText('.stagger-text')
+
+gsap.registerPlugin(SplitText)
